e0c8a8215b95ea47546692463bc8c6c0b6509499
Grigoriy <castfrontend@gmail.com>
2019-01-21 15:54:42 +0400

    add multi pages config

---
+++ src/pages/homePage/index.js
@@ -0,0 +1,28 @@
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// import cryptoInfo from '../../js/cryptoInfo';
+// import Todo from '../../js/todo';
+// import './scss/App.scss';
+
+// import { MDCRipple } from '@material/ripple/index';
+// import { MDCDialog } from '@material/dialog';
+
+
+// const mainRippleButton = document.getElementsByClassName('foo-button'); // Назначаем кнопку по классу для риппл эффекта
+
+// for (let i = 0; i < mainRippleButton.length; i++) {
+//     /**
+//      * Назначаем риппл эффект на нашу основную кнопку для всех кнопок на странице путём перебора циклом
+//      *  */
+//     MDCRipple.attachTo(mainRippleButton[i]);
+// };
+
+// const aa = new cryptoInfo('domElement');
+
+// aa.getCrypto();
+
+// ReactDOM.render(
+//     <Todo name="Taylor" />,
+//     document.getElementById('domElementSecond');
+// );
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+
+    // output: {
+    //     filename: 'bundle.js',
+    //     path: path.resolve(__dirname, 'dist')
+    // },
+    entry: {
+        homePage: './src/pages/homePage/index.js',
+        pageOne: './src/pages/pageOne/index.js',
+        pageTwo: './src/pages/pageTwo/index.js'
+    },
 
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        // path: __dirname + '/dist/pages'
+        path: path.resolve(__dirname, './dist/pages'),
+        // filename: '[name].js',
+        filename: './../js/[name].js',
+        publicPath: ''
+        
     },
 
     module: {
@@ -28,7 +42,6 @@ module.exports = {
                     publicPath: '../',
                     fallback: 'style-loader',
                     allChunks: true,
-
                     use: [{
                         loader: "css-loader"
                     }, {
@@ -46,8 +59,21 @@ module.exports = {
         new ExtractTextPlugin('main.css'),
         new HtmlWebpackPlugin({
             title: 'Home page',
-            filename: 'index.html',
-            template: './src/index.html'
+            filename: 'homepage/index.html',
+            template: './src/pages/homePage/index.html',
+            chunks: ['homePage']
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Page One',
+            filename: 'pageone/index.html',
+            template: './src/pages/pageOne/index.html',
+            chunks: ['pageOne']
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Page Two',
+            filename: 'pagetwo/index.html',
+            template: './src/pages/pageTwo/index.html',
+            chunks: ['pageTwo']
         })
     ],
     devServer: {
